178e8d3650eacea0c02cfbf642f5e95621d61358
Pavel2202 <paveltimenov3@gmail.com>
2023-04-06 11:06:48 +0300

    Ad MultiSigWallet

---
+++ MultiSigWallet/MultiSigWallet.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract MultiSigWallet {
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint256 public requiredApprovals;
+    Transaction[] public transactions;
+    mapping(uint256 => mapping(address => bool)) public approved;
+
+    event Deposit(address indexed sender, uint256 amount);
+    event Submit(uint256 indexed txId);
+    event Approve(address indexed owner, uint256 indexed txId);
+    event Revoke(address indexed owner, uint256 indexed txId);
+    event Execute(uint256 indexed txId);
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "only owner");
+        _;
+    }
+
+    modifier txExists(uint256 _txId) {
+        require(_txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notApproved(uint256 _txId) {
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint256 _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint256 _requiredApprovals) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _requiredApprovals > 0 && _requiredApprovals <= _owners.length,
+            "invalid required approvals"
+        );
+
+        for (uint256 i; i < _owners.length; i++) {
+            address owner = _owners[i];
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner is not unique");
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        requiredApprovals = _requiredApprovals;
+    }
+
+    function submit(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external onlyOwner {
+        transactions.push(
+            Transaction({to: _to, value: _value, data: _data, executed: false})
+        );
+        emit Submit(transactions.length - 1);
+    }
+
+    function approve(
+        uint256 _txId
+    ) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId) {
+        approved[_txId][msg.sender] = true;
+        emit Approve(msg.sender, _txId);
+    }
+
+    function execute(
+        uint256 _txId
+    ) external onlyOwner txExists(_txId) notExecuted(_txId) {
+        require(
+            _getApprovals(_txId) >= requiredApprovals,
+            "not enough approvals"
+        );
+        Transaction storage transaction = transactions[_txId];
+        transaction.executed = true;
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "tx failed");
+        emit Execute(_txId);
+    }
+
+    function revoke(
+        uint256 _txId
+    ) external onlyOwner txExists(_txId) notExecuted(_txId) {
+        require(approved[_txId][msg.sender], "tx not approved");
+        approved[_txId][msg.sender] = false;
+        emit Revoke(msg.sender, _txId);
+    }
+
+    function _getApprovals(uint256 _txId) private view returns (uint256 count) {
+        for (uint256 i; i < owners.length; i++) {
+            if (approved[_txId][owners[i]]) {
+                count++;
+            }
+        }
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+}
